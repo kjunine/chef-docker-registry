684dbd483ffc327c521eceb33df542fc3e54dae1
Daniel Ku <kjunine@gmail.com>
2014-10-07 16:16:12 +0900

    Adds initial version.

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'docker-registry'
+maintainer       'Daniel Ku'
+maintainer_email 'kjunine@gmail.com'
+license          'MIT'
+description      'Installs/Configures docker-registry'
+long_description 'Installs/Configures docker-registry'
+version          '0.1.0'
+
